c0e056b8afadc86b59e94fd4bd5badb7b63043ae
= <mwilker@ltu.edu>
2019-04-17 14:55:36 -0400

    Added Midi Interpreter and Renderer classes and did some work on the main function

---
+++ Rhythm-Jumper/Midi-Interpreter.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+static class MidiInterpreter
+{
+private:
+	class MidiData
+	{
+
+	};
+public:
+	static MidiData* InterpretMidi(std::string fileName);
+};
--- Rhythm-Jumper/Rhythm-Jumper.cpp
+++ Rhythm-Jumper/Rhythm-Jumper.cpp
@@ -1,10 +1,58 @@
-// Rhythm-Jumper.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
-int main()
+#include <cstdlib>
+#include <SDL.h>
+#include <SDL_thread.h>
+#include <stdio.h>
+#include <iostream>
+#define SCREEN_WIDTH 1920
+#define SCREEN_HEIGHT 1080
+int draw(void* ptr);
+int game(void* ptr);
+int draw(void* ptr)
 {
-    return 0;
+	while (true)
+	{
+		std::cout << "This thread functions.";
+		SDL_Delay(rand() % 100);
+	}
+	return 0;
 }
-
+int game(void* ptr)
+{
+	while (true)
+	{
+		std::cout << "GameThread";
+		SDL_Delay(rand() % 100);
+	}
+	return 0;
+}
+int main(int argc, char* args[]) 
+{
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+	if (SDL_Init(SDL_INIT_EVERYTHING) < 0) 
+	{
+		fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+		return 1;
+	}
+	window = SDL_CreateWindow(
+		"Rhythm-Jumper",
+		SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+		SCREEN_WIDTH, SCREEN_HEIGHT,
+		SDL_WINDOW_SHOWN
+	);
+	if (window == NULL)
+	{
+		fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+	SDL_Thread* drawThread = SDL_CreateThread(draw, NULL, NULL);
+	SDL_Thread* gameThread = SDL_CreateThread(game, NULL, NULL);
+	screenSurface = SDL_GetWindowSurface(window);
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0x00, 0x00, 0x00));
+	SDL_UpdateWindowSurface(window);
+	SDL_Delay(2000);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	return 0;
+}
